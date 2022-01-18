59d9fea93af72c79eeca74335828eb55d56f0ee1
Joseph <manueljosephtrabajo@gmail.com>
2022-01-18 17:33:38 -0600

    Add 3 files news: Angular.html, Angular_logic.js and Angular_styles.css

---
+++ Angular_logic.js
@@ -0,0 +1,49 @@
+//declaracion del Modulo
+
+let app = angular.module("Angular_App", [])
+
+/*
+| Dentro del [] se importan los modulos de Angular.
+| Existe un modulo que se llama ngResource que es para consumir apis Rest
+
+*/
+
+//declaracion del Controlador
+
+app.controller("ControllerMaster1",["$scope" ,function($) {
+  $.name= "Joseph";
+  $.newComment= {}
+  $.comments= [
+
+    {
+      comment: "excelent tutorial",
+      username: "Manuel",
+      id_user: 058
+    },
+
+    {
+      comment: "bad tutorial",
+      username: "var_02",
+      id_user: 25
+    }
+  
+  ]
+
+  $.addComment = function () {
+    $.comments.push($.newComment);
+    $.newComment= {};
+  }
+}]);
+
+/*
+% Angular es MVVM (Modelo-Vista-Vista-Modelo)
+*/
+
+// two way data binding and ng Modelo
+
+/*
+) data binding es la sincronización que hay entre el modelo y la vista
+*/
+
+
+//dependency injections
